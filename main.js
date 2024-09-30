787f26665b704c86b628359357ad3cb45e32b04c
nemethkoppany <koppany344@gmail.com>
2024-09-30 19:19:19 +0200

    comments, customized to my liking, new feature: If the reuslt is 'Error', you don't have to clear, you can type normally, Currently sqrt and pow are not working

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-const display  = document.getElementById("display");
+const display  = document.getElementById("display"); //Egy display nevű változóban eltároljuk a display id-jű input adatait.
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input){ //Függvény egy bemeneti paramétert vár, az input értéket szeretnénk hozzáfűzni a display-hez
+   if(display.value == 'Error'){
+    display.value = '';
+   }
+    display.value += input; //A display értékéhez hozzácsatolja az inputot. Például: display.value = 5,input = "+", input = 7 --> 5 + 7
 }
 
-function clearDisplay(){
-    display.value = "";
+function clearDisplay(){//Ez a függvény a C, azaz Clear gombhoz van hozzárendelve
+    display.value = "";//Amikor a C gomb lenyomásra kerül a display értéke egy üres karakter, azaz semmi lesz.
 }
 
-function calculate(){
-    try{display.value = eval(display.value);
+function calculate(){//A számológép számolásáért felelős függvény
+    
+    if(display.value == '√'){
+        try{display.value = Math.sqrt(display.value)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+    if(display.value == 'x^2'){
+        try{display.value = Math.pow(display.value, 2)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+}  
+    try{display.value = eval(display.value);//Az Eval egy beépített operátor ami kiszámolja (esetünkben) az display-en lévő számokat, ez az operátor képes felismerni az számtani jeleket.
+                                            //Ez az egész egy try-catch függvényben van. Ez megpróbálja elvégezni a műveletet(try) és Ha valamilyen probléma van, például nem fejeztük be a műveletet (3 - ) akkor azt a hibát elkapja(catch) 
+                                            //és a display értéke 'Error' lesz.
 
     }catch(error){
         display.value = "Error";
     }
 
-  
+
+    if(display.value.includes( '√')){
+        try{display.value = Math.sqrt(display.value)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+    if(display.value.includes( 'x^2')){
+        try{display.value = Math.pow(display.value, 2)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+}
 }
