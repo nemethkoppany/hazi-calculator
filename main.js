83a333b7be230ae01001f2410757a4012e0f7096
nemethkoppany <koppany344@gmail.com>
2024-09-30 20:04:51 +0200

    sqrt and pow now works

--- main.js
+++ main.js
@@ -12,22 +12,32 @@ function clearDisplay(){//Ez a függvény a C, azaz Clear gombhoz van hozzárend
 }
 
 function calculate(){//A számológép számolásáért felelős függvény
-    
-    if(display.value == '√'){
-        try{display.value = Math.sqrt(display.value)
+ //-------------------------------------------------------------------------------
+ //Új funkciók: gyökvonás és négyzetre emelés
+ //Ezeket nem támogatja az eval()
+ if (display.value.includes('√')) {//Ha rajta van a displayen
+    display.value = parseInt(display.value);//convertálja át a stringet int-té(szövegből számmá)
+    try {
+      display.value = Math.sqrt(display.value); //A display értéke legyen a megadott szám gyöke.
+    } catch (error) { //Ha hibába ütközik írja ki azt, hogy 'Error'.
+      display.value = "Error";
     }
-    catch(error){
-        display.value = "Error";
-    }
+  }
+  
 
-    if(display.value == 'x^2'){
-        try{display.value = Math.pow(display.value, 2)
+  //Ez ugyan az mint a gyökvonás, csak gyökvonás helyett négyzetre emelünk.
+  if (display.value.includes('x^2')) {
+    display.value = parseInt(display.value);
+    try {
+      display.value = Math.pow(display.value, 2); 
+    } catch (error) {
+      display.value = "Error";
     }
-    catch(error){
-        display.value = "Error";
-    }
-}
-}  
+  }
+ //-------------------------------------------------------------------------------
+
+
+
     try{display.value = eval(display.value);//Az Eval egy beépített operátor ami kiszámolja (esetünkben) az display-en lévő számokat, ez az operátor képes felismerni az számtani jeleket.
                                             //Ez az egész egy try-catch függvényben van. Ez megpróbálja elvégezni a műveletet(try) és Ha valamilyen probléma van, például nem fejeztük be a műveletet (3 - ) akkor azt a hibát elkapja(catch) 
                                             //és a display értéke 'Error' lesz.
@@ -36,20 +46,8 @@ function calculate(){//A számológép számolásáért felelős függvény
         display.value = "Error";
     }
 
+}
 
-    if(display.value.includes( '√')){
-        try{display.value = Math.sqrt(display.value)
-    }
-    catch(error){
-        display.value = "Error";
-    }
 
-    if(display.value.includes( 'x^2')){
-        try{display.value = Math.pow(display.value, 2)
-    }
-    catch(error){
-        display.value = "Error";
-    }
-}
-}
-}
+
+
